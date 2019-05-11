c7fc70bf9e513f475c4bd610f72b7cd13eab4e7b
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-05-11 19:50:12 +0000

    enable embed-thumbnail

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 while read -r p; do
   echo "---- Downloading song: $p ----"
   youtube-dl --rm-cache-dir
-  youtube-dl "$p" --extract-audio --audio-format mp3 --add-metadata --audio-quality 5 -o "%(title)s.%(ext)s"
+  youtube-dl "$p" --extract-audio --audio-format mp3 --add-metadata --audio-quality 4 --embed-thumbnail -o "%(title)s.%(ext)s"
 done <songs.txt
 
 
