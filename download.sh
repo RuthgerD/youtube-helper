3b232dabbc7421f4fa0a7846ea39989c0e67a25a
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-05-11 19:18:37 +0000

    also use .txt for linux

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@ while read -r p; do
   echo "---- Downloading song: $p ----"
   youtube-dl --rm-cache-dir
   youtube-dl $p --extract-audio --audio-format mp3 --add-metadata --audio-quality 5 -o "%(title)s.%(ext)s"
-done <songs
+done <songs.txt
 
 
