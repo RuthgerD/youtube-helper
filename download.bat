2a2ee5142dedfe42221339583f43f669f3fa9671
Ruthger Dijt <ruthgerdijt@gmail.com>
2019-05-11 19:41:40 +0000

    Update download.bat

--- download.bat
+++ download.bat
@@ -2,5 +2,5 @@
 for /F "usebackq tokens=*" %%A in ("songs.txt") do (
   echo ---- Downloading song: %%A ----
   youtube-dl.exe --rm-cache-dir
-  youtube-dl.exe %%A --extract-audio --audio-format mp3 --add-metadata --audio-quality 5 -o "%(title)s.%(ext)s"
+  youtube-dl.exe "%%A" --extract-audio --audio-format mp3 --add-metadata --audio-quality 5 -o "%(title)s.%(ext)s"
 )
